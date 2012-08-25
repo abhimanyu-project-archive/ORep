3e6a29981f2ae4c48dbe22a62fb1669a1a6578de
abhimanyuma <abhimanyu.m.a@gmail.com>
2012-08-25 15:52:15 +0530

    Bug fix

--- mysql_class.php
+++ mysql_class.php
@@ -67,7 +67,7 @@ class mysql
             if ($arg_count == 2) {
                 $what = func_get_arg(0);
                 $extra = func_get_arg(1);
-                $query = "SELECT " . $what . " FROM " . $this->table . " " . $extra . ";";
+                $query = "SELECT " . $what . " FROM " . $this->table . " WHERE " . $extra . ";";
                 return $this->execute($query);
             }//select("*","sitename=gwgguwuww,jwjwjw")
     }
