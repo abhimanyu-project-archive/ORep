b236da97b0aec32a459da9edba841fec8d776861
root <hripat1989@gmail.com>
2012-08-26 05:08:50 +0530

    profile

--- logout.php
+++ logout.php
@@ -4,6 +4,8 @@
 <link href="css/bootstrap.css" rel="stylesheet">
 <?php
 $_SESSION['username']=NULL;
+$_POST["username"]=NULL;
+$_POST["password"]=NULL;
 echo "Logged Out!";
 echo "<a href='profile.php'>Go to Login Page</a>";
 ?>
