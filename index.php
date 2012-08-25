05a20b83cbb0b15822a351193a9d97a21c91e8e0
abhimanyuma <abhimanyu.m.a@gmail.com>
2012-08-25 12:41:56 +0530

    Database functions added

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
     <body>
     <h1>Hello, world!</h1>
     <script src="js/bootstrap.min.js"></script>
+    <script src="http://yui.yahooapis.com/3.6.0/build/yui/yui-min.js"></script>
     </body>
     </html>
