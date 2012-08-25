5771ff184f81228d63e35a69ba3dc85f8afaa763
Ashu <ashu@Ashu-Mint.(none)>
2012-08-25 16:20:05 +0530

    bug fix

--- login.php
+++ login.php
@@ -2,20 +2,82 @@
     <head>
     <title>Bootstrap 101 Template</title>
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<?php
+		require_once('connect_db.php');
+		require_once('mysql_class.php');
+	?>
+
+
+	<?php
+
+		$user = $_POST["username"];
+		$pass = $_POST["password"];
+		$ssid = $_GET["ssid"];
+		
+		$ssidaccess = new mysql("ssidtable");
+		$parameter = "ssid=".$ssid;
+
+		$result = $ssidaccess -> select("*", $parameter);
+		//$row = mysql_fetch_array($result);
+		if(!empty($result))
+		{
+			$row = mysql_fetch_array($result);
+			$siteid = $row["siteid"];
+			$parameter = "ssid=".$ssid;
+			$ssidaccess -> delete($parameter);
+
+			if(!(empty($user) || empty($password)))
+			{
+				$dbaccess = new mysql("userinfo");
+				$parameter = "username=".$user;
+				$result = $dbaccess -> select("*", $parameter);
+	
+				$row = mysql_fetch_array($result);
+				if($row["passwordhash"]==$pass)
+				{
+					//user authenticated
+					$userid = $row["userid"];
+					$dbaccess2 = new mysql("permission");
+					
+
+					while(1)
+					{
+						$siteuserid = random_gen(20);
+
+						$parameter = "siteuserid=".$siteuserid;
+						$result = $dbaccess2 -> select("*", $parameter);
+
+						$row = mysql_fetch_array($result);
+
+						if(empty($row["siteuserid"]))
+						{
+							$dbaccess2 -> insert(array($siteid, $userid, $siteuserid));
+						}
+					}
+				}
+
+				
+			}
+		}
+
+	?>
     </head>
     <body>
+	
+
     <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
-<fieldset >
-<legend>Login</legend>
-<input type='hidden' name='submitted' id='submitted' value='1'/>
-<label for='username' >UserName*:</label>
-<input type='text' name='username' id='username'  maxlength="50" />
-<label for='password' >Password*:</label>
-<input type='password' name='password' id='password' maxlength="50" />
-<input type='submit' name='Submit' value='Submit' />
-</fieldset>
-</form>
+	<fieldset >
+		<div id = "loginform" align="center">
+		<legend>Login</legend>
+		<input type='hidden' name='submitted' id='submitted' value='1'/>
+		UserName*:<input type='text' name='username' id='username'  maxlength="50"/>
+		Password*:<input type='password' name='password' id='password' maxlength="50"/>
+		<br>
+		<input type='submit' name='Submit' value='Submit'/>
+		</div>
+	</fieldset>
+    </form>
     <script src="js/bootstrap.min.js"></script>
     <script src="http://yui.yahooapis.com/3.6.0/build/yui/yui-min.js"></script>
     </body>
