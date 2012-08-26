9cb07c5a0511406437cd59aa50fa40401103f823
root <hripat1989@gmail.com>
2012-08-26 05:51:02 +0530

    profile

--- logout.php
+++ logout.php
@@ -6,8 +6,8 @@
 session_start();
 unset($_SESSION['username']);
 echo "<div align='center'>";
-echo "<cite>Logged Out!</cite>";
-echo "<br><strong><a href='profile.php'>Go to Login Page</a></strong>";
+echo "<br><br><br><cite>Logged Out!</cite>";
+echo "<br><br><strong><a href='profile.php'>Go to Login Page</a></strong>";
 echo "</div>";
 ?>
 </head>
